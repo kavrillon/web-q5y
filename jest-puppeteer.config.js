ca16e55fc023f3f0800a6569e469001fcd4c9d7f
Kevin Avrillon <kevin@avrillon.me>
2019-11-11 16:30:12 +0100

    feat: manage url mode

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -2,17 +2,22 @@ const DEBUG_MODE = process.argv.includes('--debug');
 const fileMethods = require('./test/utils/file');
 const config = fileMethods.getRCConf('./.q5yrc');
 
-module.exports = {
+let puppeteer = {
   launch: DEBUG_MODE
     ? {
         headless: true,
         slowMo: 100
       }
-    : {},
-  server: {
+    : {}
+};
+
+if (config.command && config.command.length > 0) {
+  puppeteer.server = {
     command: config.command,
     port: config.port,
     launchTimeout: 120000,
-    debug: false
-  }
-};
+    debug: DEBUG_MODE
+  };
+}
+
+module.exports = puppeteer;
